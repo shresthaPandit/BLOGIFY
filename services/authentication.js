97cea7010580ee8cea3cea1a75632f415862eaa4
shrestha <happygrea123411@gmail.com>
2025-05-20 12:12:42 +0530

    Use JWT_SECRET from environment variables

--- services/authentication.js
+++ services/authentication.js
@@ -1,19 +1,19 @@
-const JWT=require("jsonwebtoken");
-const secret="gu@1234";
+const JWT = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET || "gu@1234";
 
-function createTokenForUser(user){
-    const payload={
+function createTokenForUser(user) {
+    const payload = {
         _id: user._id,
-        email:user.email,
-        profileImage:user.profileImage,
-        role:user.role,
-    }
-    const token=JWT.sign(payload,secret);
+        email: user.email,
+        profileImage: user.profileImage,
+        role: user.role,
+    };
+    const token = JWT.sign(payload, secret);
     return token;
 }
-function validate(token){
-    const payload=JWT.verify(token,secret);
+function validate(token) {
+    const payload = JWT.verify(token, secret);
     return payload;
 }
 
-module.exports={createTokenForUser,validate};
+module.exports = { createTokenForUser, validate };
